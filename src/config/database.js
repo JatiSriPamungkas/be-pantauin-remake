511bcca5536131f95ae7438c808b92940d30af81
Jati Sri Pamungkas <jatispamungkas357@gmail.com>
2025-10-05 14:24:36 +0700

    feat: initialize config databases

---
+++ src/config/database.js
@@ -0,0 +1,14 @@
+import mysql from "mysql2/promise";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const pool = mysql.createPool({
+    host: process.env.EXPRESS_HOST,
+    user: process.env.EXPRESS_USER,
+    password: process.env.EXPRESS_PASSWORD,
+    database: process.env.EXPRESS_DATABASE,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+});
